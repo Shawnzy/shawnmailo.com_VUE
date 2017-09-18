fac37862ae0b61387ae9594439974f6dd136cb2a
Shawn <mailo.shawn@gmail.com>
2017-09-18 01:31:58 -0300

    test

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -1,5 +1,6 @@
 <template>
   <h3>Resume page</h3>
+  <h4>test</h4>
   <div class="content">
     <iframe src="https://docs.google.com/viewer?srcid=0B_3AQrKxDju1bFBzMlZfa1AxMnc&pid=explorer&efh=false&a=v&chrome=false&embedded=true" width="580px" height="480px"></iframe>
   </div>
