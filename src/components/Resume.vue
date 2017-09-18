b310533e2ed4ad59de2e0de69f427e7b150acfd5
Shawn <mailo.shawn@gmail.com>
2017-09-18 01:28:00 -0300

    added resume

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -1,3 +1,6 @@
 <template>
-<h3>Resume page</h3>
+  <h3>Resume page</h3>
+  <div class="content">
+    <iframe src="https://docs.google.com/viewer?srcid=0B_3AQrKxDju1bFBzMlZfa1AxMnc&pid=explorer&efh=false&a=v&chrome=false&embedded=true" width="580px" height="480px"></iframe>
+  </div>
 </template>
